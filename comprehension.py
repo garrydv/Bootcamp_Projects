aa02145ac487e1cb0c01d1e4161d5df034f24668
garrydv <igordv@icloud.com>
2024-06-12 18:53:17 -0700

    Comprehension moved

---
+++ comprehension.py
@@ -0,0 +1,12 @@
+def myfunc(*args):
+    return [arg for arg in args if arg % 2 == 0]
+
+
+"""
+def myfunc(*args):
+    even_numbers = []
+    for arg in args:
+        if arg % 2 == 0:
+            even_numbers.append(arg)
+    return even_numbers
+"""
