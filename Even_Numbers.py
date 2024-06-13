5124054f29c5fd52c450c5015375ab4e03ebfefc
garrydv <igordv@icloud.com>
2024-06-12 18:51:40 -0700

    Added comprehension

--- Even_Numbers.py
+++ Even_Numbers.py
@@ -9,8 +9,10 @@
 nums = [i for i in range(0,11) if i % 2 == 0]
 print(nums)
 
-#It is a comprehension for
+"""
+It is a comprehension for
 
-#for i in range(0,11):
-#    if i % 2 == 0:
-#        print(i)
+for i in range(0,11):
+    if i % 2 == 0:
+        print(i)
+"""
