95e0487cd4e76a5bf2811b74e8186706277a27c1
garrydv <igordv@icloud.com>
2024-06-13 12:44:24 -0700

    Latest update for Jun 13th

--- Lesser_Of_Two_Evens.py
+++ Lesser_Of_Two_Evens.py
@@ -5,7 +5,6 @@ lesser_of_two_evens(2,4) --> 2
 lesser_of_two_evens(2,5) --> 5
 """
 def lesser_of_two_evens(a,b):
-
     if a % 2 == 0 and b % 2 == 0:  # both numbers are even
         return min(a, b)
     else:  # at least one number is odd
