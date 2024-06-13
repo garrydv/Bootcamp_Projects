b8ebc239858ad715ac6e48e36b04e394e3a3333c
garrydv <igordv@icloud.com>
2024-06-13 12:43:19 -0700

    Warmup solved with min and max

--- Lesser_Of_Two_Evens.py
+++ Lesser_Of_Two_Evens.py
@@ -1,8 +1,15 @@
 """
-
-function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
+function that returns the lesser of two given numbers if both numbers are even,
+but returns the greater if one or both numbers are odd
 lesser_of_two_evens(2,4) --> 2
 lesser_of_two_evens(2,5) --> 5
-
-
 """
+def lesser_of_two_evens(a,b):
+
+    if a % 2 == 0 and b % 2 == 0:  # both numbers are even
+        return min(a, b)
+    else:  # at least one number is odd
+        return max(a, b)
+
+# Testing function:
+print(lesser_of_two_evens(2,5))
