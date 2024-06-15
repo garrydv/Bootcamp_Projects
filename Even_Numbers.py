f51b3f633ecaa19a41ab908be146fda78278de03
garrydv <igordv@icloud.com>
2024-06-14 20:30:41 -0700

    Level 1 completed

--- Even_Numbers.py
+++ Even_Numbers.py
@@ -1,32 +1,25 @@
 # 1. Print even numbers from given range
 
-
-"""Most basic solution
-
+# Most basic solution
+"""
 for x in range(0,11,2):
     print(x)
 """
 
-#More advanced solution:
+# More advanced solution:
 
 nums = [i for i in range(0,11) if i % 2 == 0]
 print(nums)
 
+# Comprehension
 """
-It is a comprehension for
-
 for i in range(0,11):
     if i % 2 == 0:
         print(i)
 """
 
-
+# 2.Similar problem: numbers divided by 3 ***
 """
-            ***************
-
-*** Similar problem: numbers divided by 3 ***
-
 nums = [i for i in range(1,51) if i % 3 == 0]
 print(nums)
-
 """
--- True_Falls_If_Less_Or_More.py
+++ True_Falls_If_Less_Or_More.py
@@ -23,5 +23,16 @@ print (makes_twenty(15,5))
 """
 # Comprehension:
 
-def makes_twenty(n1, n2):
-    return n1 == 20 or n2 == 20 or n1 + n2 == 20
+"""def makes_twenty(n1, n2):
+    return n1 == 20 or n2 == 20 or n1 + n2 == 20"""
+
+# 2. Return True if n is within 10 of either 100 or 200
+
+def almost_there(n):
+    return (abs(100 - n) <= 10) or (abs(200 - n) <= 10)
+# Check1
+print(almost_there(90))
+# Check2
+print(almost_there(150))
+# Check3
+print(almost_there(209))
